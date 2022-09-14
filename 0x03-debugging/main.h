f5d2ae37830e744180173693c51d216ca420ca6f
magnes158 <mohamedabdiali158@gmail.com>
2022-09-14 05:50:47 -0700

    main1

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,36 +1,8 @@
-#include "main.h"
+#ifndef MAIN_H
+#define MAIN_H
 
-/**
-* print_remaining_days - takes a date and prints how many days are
-* left in the year, taking leap years into account
-* @month: month in number format
-* @day: day of month
-* @year: year
-* Return: void
-*/
+#include <stdio.h>
 
-void print_remaining_days(int month, int day, int year)
-{
-if ((year % 400 == 0) || (year % 4 == 0 && year % 100 !=  0))
-{
-if (month >= 3 && day >= 60)
-{
-day++;
-}
+void positive_or_negative(int i);
 
-printf("Day of the year: %d\n", day);
-printf("Remaining days: %d\n", 366 - day);
-}
-else
-{
-if (month == 2 && day == 60)
-{
-printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
-}
-else
-{
-printf("Day of the year: %d\n", day);
-printf("Remaining days: %d\n", 365 - day);
-}
-}
-}
+#endif /* MAIN_H */
