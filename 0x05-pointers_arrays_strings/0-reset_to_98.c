f31653cd7313f651f82d165184498f6303db7636
magnes158 <mohamedabdiali158@gmail.com>
2022-09-18 23:14:36 -0700

    j

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,22 +1,11 @@
 #include "main.h"
-
 /**
- *  * _strcat - a function that concatenates two strings
- *   * @dest: copy to
- *    * @src: copy from
- *     * Return: two concat strings
- *      */
-char *_strcat(char *dest, char *src)
+ *  * reset_to_98 - this function reset the number
+ *   * @n: is the pointer
+ *    *  Return: Always 0
+ *     */
+
+void reset_to_98(int *n)
 {
-		int i, n;
-
-			for (i = 0; dest[i] != '\0'; i++)
-					{
-							}
-
-				for (n = 0; (dest[i + n] = *src++) != '\0'; n++)
-						{
-								}
-
-					return (dest);
+		*n = 98;
 }
