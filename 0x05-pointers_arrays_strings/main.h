28d7b8cf89f54e9b3faf8e63f22c513364832a8c
magnes158 <mohamedabdiali158@gmail.com>
2022-09-19 08:55:46 -0700

    for

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,11 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/**
-* void prototypes()
-* int prototypes()
-*/
+#include <stdio.h>
 
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -16,6 +14,6 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
-int _atoi(char *str);
+int _atoi(char *s);
 
-#endif /*MAIN_H*/
+#endif /* MAIN_H */
