b3410eb70b1cff46140ef3911d3c09fd1080955d
magnes158 <mohamedabdiali158@gmail.com>
2022-11-02 15:04:59 -0700

    0x15

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -238,7 +238,7 @@ void print_entry(unsigned long int e_entry, unsigned char *e_ident)
 
 	if (e_ident[EI_CLASS] == ELFCLASS32)
 		printf("%#x\n", (unsigned int)e_entry);
-AOA
+
 	else
 		printf("%#lx\n", e_entry);
 }
